f81e4d415ecfb2329cbd83973b86228519e247e2
SHIVAM KUMAR <145523549+shivamshrii@users.noreply.github.com>
2024-08-03 12:10:16 +0000

    String Methods

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,38 @@
+/*
+const name = "Shivam-shri" 
+const repoCount = 40
+
+// console.log((name + repoCount + " Value"));
+
+console.log(`Hello my name is ${name} and my repo count is  ${repoCount}`);
+
+const gameName = new String('Shivam')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('i'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-3,4)
+console.log(anotherString);
+*/
+
+const newStringOne = "     shivam    "
+console.log(newStringOne.trim());
+
+
+const url = "https://shivam.com/shivam%20shri"
+console.log(url.replace('%20', '-'));
+console.log(url.includes('shivam'));
+console.log(url.includes('shiva'));
+console.log(url.includes('raja'));
+
+
+ 
+
