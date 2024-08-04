cef95aa4ad54d7de2fb7a3a8d21abf835d05d5be
SHIVAM KUMAR <145523549+shivamshrii@users.noreply.github.com>
2024-08-04 10:37:46 +0000

    json talk

---
+++ 02_Basics/05_objects.js
@@ -0,0 +1,15 @@
+//Destructuring
+
+const course = {
+    coursename: "Js in Hindi",
+    price: 999,
+    courseteacher: "mahadev"
+}
+const {courseteacher: teacher} = course
+console.log(teacher);
+
+[
+    {},
+    {},
+    {},
+]
