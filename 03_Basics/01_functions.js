95fd8e49d23e320f845508f806a55b01efecd286
SHIVAM KUMAR <145523549+shivamshrii@users.noreply.github.com>
2024-08-04 11:01:52 +0000

    How To Create Function

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,31 @@
+function sayMyName(){
+    console.log("S");
+    console.log("h");
+    console.log("i");
+    console.log("v");
+    console.log("a");
+}
+
+// sayMyName()
+
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+     
+// }
+// addTwoNumbers(2, 2)
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result 
+     
+    return number1 + number2
+}
+const result = addTwoNumbers(2, 2)
+// console.log("Result", result)
+
+function userLoginMessage(username){
+    return '${username} just logged in'
+}
+// console.log(userLoginMessage("shivam"))
+
