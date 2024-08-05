22b108f41763ccafb4f5bb1b9d3931d0246c549d
SHIVAM KUMAR <145523549+shivamshrii@users.noreply.github.com>
2024-08-05 11:24:30 +0000

    use of arrow

---
+++ 03_Basics/03_arrows.js
@@ -0,0 +1,60 @@
+// const user = {
+//     username: "shivam",
+//     price: 999,
+
+//     welcomeMessage: function(){
+//         console.log(`${this.username} , welcome to website`);
+        
+//     }
+// }
+
+// user.welcomeMessage()
+// user.username = "shiva"
+// user.welcomeMessage()
+// console.log(this);
+
+
+
+// function chai(){
+//     let usename = "shivam"
+//     console.log(this.usename);
+    
+// }
+// chai()
+
+
+// const chai = function()
+// {
+//     let usename = "shivam"
+//     console.log(this.username);
+// }
+
+// const chai = () =>
+// {
+//     let usename = "shivam"
+//     console.log(this.username);
+// }
+// const chai = function()
+// {
+//     let usename = "shivam"
+//     console.log(this);
+// }
+
+// const addTwo = (num1, num2) => {
+//     console.log(3,4);
+    
+// }
+
+
+// const addTwo = (num1, num2) => num1 + num2
+// console.log(addTwo(3,4));
+
+
+// const addTwo = (num1, num2) => (num1 + num2)
+// console.log(addTwo(3,4));
+
+const addTwo = (num1, num2) => {("username: shivam")}
+console.log(addTwo(3,4));
+
+
+
