6adbe32f743ca1ed3456c28fec6812bf397e158d
SHIVAM KUMAR <145523549+shivamshrii@users.noreply.github.com>
2024-08-05 12:28:45 +0000

    if statement

---
+++ 04_conditionalFlow/one.js
@@ -0,0 +1,33 @@
+// if
+
+// const score = 200 
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+
+// const balance = 1000
+
+// // if(balance > 500) console.log("test");
+
+// if(balance < 500){
+//     console.log("less than");
+
+// } else if(balance < 750 ){
+//     console.log("less than 750")
+// }
+// else if(balance < 900){
+//     console.log("less than 950");
+    
+// } else{
+//     console.log("less than 1200");
+    
+// }
+
+
+
+const userLoggedIn = true 
+const debitCard = true 
+if(userLoggedIn && debitcard )
