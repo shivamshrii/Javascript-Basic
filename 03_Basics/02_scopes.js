8e36c28d67a5817c6ae832ec3be152e783839a6f
SHIVAM KUMAR <145523549+shivamshrii@users.noreply.github.com>
2024-08-05 11:01:56 +0000

    using of scopes

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -4,10 +4,53 @@ if(true){
     let a = 10
     const b = 20
     var c = 40
-    console.log("INNER:", a);
+    // console.log("INNER:", a);
     
 }
 
 // console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const usename = "shivam"
+    function two(){
+        const website = "youtube"
+        // console.log(usename);
+        
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+
+if(true){
+    const username = "shivam"
+    if (username ==="shivam"){
+        const website = "shivam"
+        // console.log(username + website);
+        
+    }
+// console.log(website);
+    
+}
+// console.log(username);
+
+
+
+// ++++++++++++++++++++++++Interesting++++++++++++++++
+
+function addone(num){
+    return num + 1
+
+}
+
+console.log(addone(5));
+
+
+const addtwo = function(num){
+    return num+2
+}
+
+addtwo(5)
